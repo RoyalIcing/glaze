f709cf28f712654ef8c5def064d1ed8546e1acd0
Patrick Smith <pgwsmith@gmail.com>
2013-07-21 01:18:14 +1000

    Improved and renamed some stuff. Added array of strings for class attributes.

--- README.md
+++ README.md
@@ -1,33 +1,37 @@
 glaze
 =====
 
-Simple PHP functions for escaping text, URLs, email addresses, and HTML attributes. Aims to be very simple and easy to read.
+Easy to use PHP functions for displaying *stuff*. Escapes text & URLs, email addresses, and HTML attributes.
 
 ```php
 // Simple text
 echo glazeText('Text to display');
 
-// Escape an email address, for making it harder for spam bots to pick up.
-$emailAddressDisplay = glazeEncodedText('email@example.com');
+// Encode an email address to make it harder for spam bots to pick up.
+$emailAddressDisplay = glazeNumericEntityEncodedText('email@example.com');
 ```
 
 For escaping HTML attributes:
 
 ```php
 ?>
-<a<?= glazeAttribute('href', 'http://www.facebook.com/', 'URL') ?>><?= glazeText('Friends & family contact me here.') ?></a>
+<a<?= glazeAttribute('href', 'http://www.facebook.com/') ?>><?= glazeText('Friends & family contact me here.') ?></a>
 <?php
 ```
 	
-Using JSON information:
+More complex example:
 
 ```php
-$info = json_decode($filePath, true); // Decode as an array
+$info = json_decode($filePath, true); // true: decodes as an array
+
+$classNamesArray = array('item', 'book');
+$classNamesArray[] = $info['genre']; // e.g. 'thriller'
 ?>
 <div<?php
 echo glazeAttribute('id', $info['itemID']);
-echo glazeAttributeCheck('data-sales-count', $info['salesCount']); // Only display the attribute if value $info['salesCount'] is not empty.
-echo glazeAttributeCheck('class', $info['isBestSeller'], 'bestSeller'); // Only displays the attribute, using the value 'bestSeller', if $info['isBestSeller'] is not empty.
+echo glazeAttribute('class', $classNamesArray); // Works with an array of values for class attributes.
+echo glazeAttributeCheck('data-sales-count', $info['salesCount']); // Only display the attribute if value $info['salesCount'] has something there.
+echo glazeAttributeCheck('selected', $info['selected'], 'selected'); // Only displays the attribute, with the value 'selected', if $info['selected'] is true.
 ?>>
 <?= glazeText($info['itemDescription']) ?>
 </div>
@@ -38,5 +42,5 @@ Using already escaped information:
 
 ```php
 $escapedText = 'Bangers &amp; Mash';
-echo glazeAttribute('alt', $escapedText, 'glazed');
+echo glazeAttribute('alt', $escapedText, GLAZE_TYPE_PREGLAZED);
 ```
--- glaze.php
+++ glaze.php
@@ -6,10 +6,18 @@ This content is released under the MIT License: http://opensource.org/licenses/M
 
 */
 
+define ('GLAZE_TYPE_TEXT', 'text');
+define ('GLAZE_TYPE_URL', 'URL');
+define ('GLAZE_TYPE_NUMERIC_ENTITY_ENCODED', 'numericEntityEncoded');
+define ('GLAZE_TYPE_SPACED_LIST_ATTRIBUTE', 'spacedListAttribute');
+define ('GLAZE_TYPE_PREGLAZED', 'preglazed');
+
+
 function glazeText($string)
 {
 	// Convert to UTF-8 if necessary.
 	$string = mb_convert_encoding($string, 'UTF-8', mb_detect_encoding($string));
+	// Encode quotes too; this function covers general and attribute text in one.
 	return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
 
@@ -18,43 +26,110 @@ function glazeURL($string)
 	return glazeText($string);
 }
 
-function glazeEncodedText($string)
+function glazeNumericEntityEncodedText($string)
 {
-	// From http://github.com/mdesign/md.spam_me_not.ee_addon/blob/master/plugins/pi.md_spam_me_not.php
+	// Based on http://github.com/mdesign/md.spam_me_not.ee_addon/blob/master/plugins/pi.md_spam_me_not.php
 	$stringLength = strlen($string);
 	$stringDisplay = '';
 	for ($i = 0; $i < $stringLength; $i++) {
 		$method = rand(1, 2);
 		$characterOrdinal = ord($string[$i]);
-		$encodedCharacter = '&#' .( ($method == 1)? $characterOrdinal : 'x' .dechex($characterOrdinal) ). ';';
+		$encodedCharacter = '&#' .( ($method === 1)? $characterOrdinal : 'x' .dechex($characterOrdinal) ). ';';
 		$stringDisplay .= $encodedCharacter;
 	}
 	return $stringDisplay;
 }
 
-function glazeValue($string, $valueType = null)
+/* Numbers */
+
+function glazeNumberWithOrdinals($number)
 {
-	if ($valueType === 'URL' || $valueType === 'url') {
-		return glazeURL($string);
+	$lastUnit = $number % 10;
+	$lastTen = $number % 100;
+	if ($lastUnit === 0 || $lastUnit >= 4 || ($lastTen >= 11 && $lastTen <= 13))
+		$suffix = 'th';
+	else if ($lastUnit === 1)
+		$suffix = 'st';
+	else if ($lastUnit === 2)
+		$suffix = 'nd';
+	else
+		$suffix = 'rd';
+	
+	return $number . $suffix;
+}
+
+/* Dates */
+
+function glazeFullDateTime($time)
+{
+	return strftime('%A, %e %B %Y %l:%M:%S %p', $time);
+}
+
+function glazeDayTime($time)
+{
+	$dayWithOrdinal = glazeNumberWithOrdinals(strftime('%e', $time));
+	return strftime('%A ' .$dayWithOrdinal. ', %l:%M:%S %p', $time);
+}
+
+function glazeShortDate($time, $showYear = true)
+{
+	if ($showYear)
+		return strftime('%e %B %Y', $time);
+	else
+		return strftime('%e %B', $time);
+}
+
+
+function glazeValue($value, $valueType = null)
+{
+	if ($valueType === GLAZE_TYPE_PREGLAZED) {
+		return $value;
 	}
-	else if ($valueType === 'glazed') {
-		return $string;
+	else if ($valueType === GLAZE_TYPE_URL) {
+		return glazeURL($value);
 	}
-	else {
-		return glazeText($string);
+	else if ($valueType === GLAZE_TYPE_NUMERIC_ENTITY_ENCODED) {
+		return glazeNumericEntityEncodedText($value);
 	}
+	else if ($valueType === GLAZE_TYPE_SPACED_LIST_ATTRIBUTE && is_array($value)) {
+		$value = implode(' ', $value);
+	}
+	
+	
+	return glazeText($value);
+}
+
+/* Attributes */
+
+// Used to automatically process certain named attributes.
+function glazeTypeForAttributeName($attributeName)
+{
+	$attributeName = strtolower($attributeName);
+	
+	if ($attributeName === 'class') {
+		return GLAZE_TYPE_SPACED_LIST_ATTRIBUTE;
+	}
+	else if ($attributeName === 'href' || $attributeName === 'src') {
+		return GLAZE_TYPE_URL;
+	}
+	
+	return GLAZE_TYPE_TEXT;
 }
 
 function glazeAttribute($attributeName, $attributeValue, $valueType = null)
 {
-	// Looks like | attributeName="attributeValue"|
+	if (empty($valueType)) {
+		$valueType = glazeTypeForAttributeName($attributeName);
+	}
+	
+	// Looks like | name="value"|
 	return ' '.$attributeName.'="'.glazeValue($attributeValue, $valueType).'"';
 }
 
 function glazeAttributeCheck($attributeName, &$attributeValueToCheck, $attributeValueToUse = null, $valueType = null)
 {
 	if (empty($attributeValueToCheck))
-		return;
+		return '';
 	
 	return glazeAttribute($attributeName, isset($attributeValueToUse) ? $attributeValueToUse : $attributeValueToCheck, $valueType);
 }
