57e397bf4aa34ced0c165e195c57e4b14530d9a6
Patrick Smith <pgwsmith@gmail.com>
2014-01-29 22:03:26 +1100

    Added `glazyElement()` function, for convenient display of simple html elements. Added `glazyBegin()` or `glazyClose()`for display of more in depth elements. Renamed `glazeNumberWithOrdinals()` to `garnishNumberWithOrdinals()`

--- glaze.php
+++ glaze.php
@@ -6,6 +6,8 @@ This content is released under the MIT License: http://opensource.org/licenses/M
 
 */
 
+define ('GLAZE_VERSION', '1.5');
+
 define ('GLAZE_TYPE_TEXT', 'text');
 define ('GLAZE_TYPE_URL', 'URL');
 define ('GLAZE_TYPE_EMAIL_ADDRESS', 'emailAddress');
@@ -54,25 +56,6 @@ function glazeEmailAddressMailtoURL($emailAddress)
 	return glazeNumericallyEncodeString($emailAddressURL);
 }
 
-/* Numbers */
-
-function glazeNumberWithOrdinals($number)
-{
-	$lastUnit = $number % 10;
-	$tens = $number % 100;
-	if ($lastUnit === 0 || $lastUnit >= 4 || ($tens >= 11 && $tens <= 13))
-		$suffix = 'th';
-	else if ($lastUnit === 1)
-		$suffix = 'st';
-	else if ($lastUnit === 2)
-		$suffix = 'nd';
-	else
-		$suffix = 'rd';
-	
-	return $number . $suffix;
-}
-
-
 function glazeValue($value, $valueType = null)
 {
 	if ($valueType === GLAZE_TYPE_PREGLAZED) {
@@ -95,10 +78,28 @@ function glazeValue($value, $valueType = null)
 	return glazeText($value);
 }
 
+/* Garnish */
+
+function garnishNumberWithOrdinals($number)
+{
+	$lastUnit = $number % 10;
+	$tens = $number % 100;
+	if ($lastUnit === 0 || $lastUnit >= 4 || ($tens >= 11 && $tens <= 13))
+		$suffix = 'th';
+	else if ($lastUnit === 1)
+		$suffix = 'st';
+	else if ($lastUnit === 2)
+		$suffix = 'nd';
+	else
+		$suffix = 'rd';
+	
+	return $number . $suffix;
+}
+
 /* Attributes */
 
-// Used to automatically process certain named attributes.
-function glazeTypeForAttributeName($attributeName)
+// Used to automatically pick out the type of certain attributes.
+/* private */ function glazeTypeForAttributeName($attributeName)
 {
 	$attributeName = strtolower($attributeName);
 	
@@ -112,17 +113,17 @@ function glazeTypeForAttributeName($attributeName)
 	return GLAZE_TYPE_TEXT;
 }
 
-function glazeAttribute($attributeName, $attributeValue, $valueType = null)
+/* private */ function glazeAttribute($attributeName, $attributeValue, $valueType = null)
 {
 	if (empty($valueType)) {
 		$valueType = glazeTypeForAttributeName($attributeName);
 	}
 	
-	// Looks like | name="value"|
-	return ' '.$attributeName.'="'.glazeValue($attributeValue, $valueType).'"';
+	// Looks like | $name="$value"|
+	return ' ' .$attributeName. '="' .glazeValue($attributeValue, $valueType). '"';
 }
 
-function glazeAttributeCheck($attributeName, &$attributeValueToCheck, $attributeValueToUse = null, $valueType = null)
+/* private */ function glazeAttributeCheck($attributeName, &$attributeValueToCheck, $attributeValueToUse = null, $valueType = null)
 {
 	if (empty($attributeValueToCheck))
 		return '';
@@ -130,24 +131,286 @@ function glazeAttributeCheck($attributeName, &$attributeValueToCheck, $attribute
 	return glazeAttribute($attributeName, isset($attributeValueToUse) ? $attributeValueToUse : $attributeValueToCheck, $valueType);
 }
 
-// Feeling Glazy?
+
+
+function glazeElementTagNameIsSelfClosing($tagName)
+{
+	$tagName = strtolower($tagName);
+	
+	switch ($tagName):
+		case 'area':
+		case 'base':
+		case 'br':
+		case 'col':
+		case 'command':
+		case 'embed':
+		case 'hr':
+		case 'img':
+		case 'input':
+		case 'keygen':
+		case 'link':
+		case 'meta':
+		case 'param':
+		case 'source':
+		case 'track':
+		case 'wbr':
+			return true;
+		default:
+			return false;
+	endswitch;
+}
+
+function glazeElementTagNameIsBlockLevel($tagName)
+{	
+	$tagName = strtolower($tagName);
+	
+	// https://developer.mozilla.org/en-US/docs/HTML/Block-level_elements
+	switch ($tagName):
+		case 'address':
+		case 'article':
+		case 'aside':
+		case 'audio':
+		case 'blockquote':
+		case 'canvas':
+		case 'dd':
+		case 'div':
+		case 'dl':
+		case 'fieldset':
+		case 'figcaption':
+		case 'figure':
+		case 'footer':
+		case 'form':
+		case 'h1':
+		case 'h2':
+		case 'h3':
+		case 'h4':
+		case 'h5':
+		case 'h6':
+		case 'header':
+		case 'hgroup':
+		case 'hr':
+		case 'noscript':
+		case 'ol':
+		case 'output':
+		case 'p':
+		case 'pre':
+		case 'section':
+		case 'table':
+		case 'tfoot':
+		case 'ul':
+		case 'video':
+			return true;
+		default:
+			return false;
+	endswitch;
+}
+
+function glazeElementTagNameBelongsInHead($tagName)
+{
+	$tagName = strtolower($tagName);
+	
+	switch ($tagName):
+		case 'title':
+		case 'meta':
+		case 'link':
+			return true;
+		default:
+			return false;
+	endswitch;
+}
+
+
+/* Feeling Glazy? */
+
+function &glazyGetOpenElements($begin = false)
+{
+	global $glazyOpenElements;
+	
+	if ($begin and !isset($glazyOpenElements)) {
+		$glazyOpenElements = array();
+	}
+	
+	return $glazyOpenElements;
+}
+
+function &glazyGetElementsBuffer($options = null)
+{
+	global $glazyElementsBuffer;
+	
+	if (isset($options['clean'])) {
+		$glazyElementsBuffer = null; // See http://au1.php.net/unset
+	}
+	
+	if (isset($options['begin'])) {
+		if (!isset($glazyElementsBuffer)) {
+			$glazyElementsBuffer = '';
+		}
+	}
+	
+	return $glazyElementsBuffer;
+}
+
+function glazyAddToElementsBuffer($string)
+{
+	$glazyElementsBuffer = &glazyGetElementsBuffer();
+	
+	if (isset($glazyElementsBuffer)) {
+		$glazyElementsBuffer .= $string;
+	}
+	else {
+		echo $string;
+	}
+}
+
+function glazyCopyAndCleanElementsBuffer()
+{
+	$glazyElementsBuffer = glazyGetElementsBuffer();
+	$copy = $glazyElementsBuffer;
+	
+	glazyGetElementsBuffer(array('clean' => true));
+	
+	return $copy;
+}
+
+/* Glazy Attributes */
+// Can be used by themselves.
 
 function glazyAttribute($attributeName, $attributeValue, $valueType = null)
 {
-	echo glazeAttribute($attributeName, $attributeValue, $valueType);
+	$string = glazeAttribute($attributeName, $attributeValue, $valueType);
+	
+	glazyAddToElementsBuffer($string);
 }
 
 function glazyAttributeCheck($attributeName, &$attributeValueToCheck, $attributeValueToUse = null, $valueType = null)
 {
-	echo glazeAttributeCheck($attributeName, $attributeValueToCheck, $attributeValueToUse, $valueType);
+	$string = glazeAttributeCheck($attributeName, $attributeValueToCheck, $attributeValueToUse, $valueType);
+	
+	glazyAddToElementsBuffer($string);
 }
 
-function glazyElement($tagName, $contentsValue, $valueType = null)
+function glazyAttributesArray($attributes)
 {
-	echo "<$tagName>";
-	echo glazeValue($contentsValue, $valueType);
-	echo "</$tagName>";
+	if (empty($attributes)):
+		return;
+	endif;
+	
+	foreach ($attributes as $attributeName => $attributeValue):
+		glazyAttribute($attributeName, $attributeValue);
+	endforeach;
 }
+
+
+function glazyElement($elementInfo, $contentsValue = null, $valueType = null)
+{
+	$attributes = array();
+	
+	if (is_string($elementInfo)):
+		// Classes
+		$parts = explode('.', $elementInfo);
+		
+		$elementInfo = $parts[0];
+		
+		if (count($parts) > 1):
+			$elementClassNameParts = array_slice($parts, 1);
+			$attributes['class'] = $elementClassNameParts;
+		endif;
+		
+		// ID
+		$parts = explode('#', $elementInfo);
+		
+		$tagName = $parts[0];
+		
+		if (count($parts) > 1):
+			$elementID = $parts[1];
+			$attributes['id'] = $elementID;
+		endif;
+	endif;
+	
+	echo "<$tagName";
+	
+	if (!empty($attributes)):
+		glazyAttributesArray($attributes);
+	endif;
+	
+	echo '>';
+	
+	if (!is_null($contentsValue)):
+		echo glazeValue($contentsValue, $valueType);
+	endif;
+	
+	if (!glazeElementTagNameIsSelfClosing($tagName)):
+		echo "</$tagName>";
+	endif;
+}
+
+
+/* private */ function glazyEnsureLatestElementOpenTagIsDisplayed()
+{
+	$glazyOpenElements = &glazyGetOpenElements();
+	if (empty($glazyOpenElements)) {
+		return;
+	}
+	
+	$latestOpenElement = &$glazyOpenElements[count($glazyOpenElements) - 1];
+	
+	if (!$latestOpenElement['openTagDone']) {
+		echo glazyCopyAndCleanElementsBuffer();
+		echo '>';
+		
+		$latestOpenElement['openTagDone'] = true;
+	}
+	
+	$latestOpenElement2 = &$glazyOpenElements[count($glazyOpenElements) - 1];
+}
+
+function glazyBegin($tagName) // $glazeType = GLAZE_TYPE_PREGLAZED
+{
+	glazyEnsureLatestElementOpenTagIsDisplayed();
+	
+	echo "<$tagName";
+	
+	$glazyElementsBuffer = &glazyGetElementsBuffer(array('begin' => true));
+	$glazyOpenElements = &glazyGetOpenElements(true);
+	
+	if (empty($glazyOpenElements)) {
+		ob_start();
+	}
+	else {
+		ob_start();
+	}
+	
+	$glazyOpenElements[] = array('tagName' => $tagName, 'openTagDone' => false);
+}
+
+function glazyClose()
+{
+	$outputtedString = ob_get_clean();
+	//$outputtedString = ob_get_contents();
+	//ob_clean();
+	
+	glazyEnsureLatestElementOpenTagIsDisplayed();
+	
+	echo $outputtedString;
+	
+	$glazyOpenElements = &glazyGetOpenElements();
+	$elementInfo = array_pop($glazyOpenElements);
+	$tagName = $elementInfo['tagName'];
+	
+	if (!glazeElementTagNameIsSelfClosing($tagName)) {
+		echo "</$tagName>";
+	}
+	
+	if (glazeElementTagNameIsBlockLevel($tagName) || glazeElementTagNameBelongsInHead($tagName)) {
+		echo "\n";
+	}
+	
+	//ob_end_flush();
+	
+	//$glazyOpenElements = glazyGetOpenElements();
+}
+
+
 
 function glazyPrintR($object)
 {
